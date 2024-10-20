83b331d8ee653b0a1d4905e406736dad9fab1b5a
Caesar101 <58193884+Caesar101@users.noreply.github.com>
2024-10-20 17:45:00 -0600

    Java sim

---
+++ ElevatorPack/Main.java
@@ -0,0 +1,38 @@
+// Author: Cesario Garcia
+// Purpose: Simulate operation of Elevator
+// Prerequisites: Elevator.java
+
+package ElevatorPack; 
+
+//utilized open source resources
+import java.io.*;
+import java.util.Scanner;
+
+//Author: Cesario Garcia
+//Purpose: Act as main script to run the elevator simulation
+//Prerequisites: Elevator.java
+
+// Elevator Sim
+
+public class Main {
+  public static void main(String[] args) {
+    //System.out.println("Hello world!");
+  
+
+    Elevator e = new Elevator();
+   
+
+      //User is notified of open door and floor number in image
+      e.openDoor();
+    // Get user input for destination
+    System.out.println("Which Floor: ");
+    Scanner s = new Scanner(System.in);
+    int dest = s.nextInt();
+    //Use user input to call pressButton method
+      e.pressButton(dest);
+      e.moveElevator(); // elevator can now move to destination floor
+    //Post move method, user is notified of their destination
+    System.out.println("You Made it to Floor: " + e.currentFloor);
+    
+  }
+}
